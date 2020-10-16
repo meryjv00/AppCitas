056fc5de1be5b67a91923d12f1cebdfa56736c2b
Mery <maria.juan.vi@gmail.com>
2020-10-16 12:54:33 +0200

    Cambios en el fichero Constantes

--- Desafio1_AppCitas/src/java/Auxiliar/Constantes.java
+++ Desafio1_AppCitas/src/java/Auxiliar/Constantes.java
@@ -12,13 +12,13 @@ package Auxiliar;
 public class Constantes {
 
     public static String BBDD = "desafio1";
-    public static String usuario = "Maria";
+    public static String usuario = "maria";
     public static String password = "Chubaca2020";
     public static String ficheroBitacora = "bitacora.txt";
     public static String rutaServidor = "/Perfiles";
     public static String tabla_usuarios = "usuarios";
     public static String tabla_roles = "roles";
-    public static String tabla_asignacion_roles = "asignacionrol";
+    public static String tabla_asignacion_roles = "asignacionRol";
     public static String tabla_preferencias = "preferencias";
-    public static String tabla_asignacion_preferencias = "asignacionpreferencias";
+    public static String tabla_asignacion_preferencias = "asignacionPreferencias";
 }
